d2767401cda0fb25dff75f6d6aefc2edbe251322
Yevhen Tkachuk <yevhen23tk@gmail.com>
2024-09-18 19:03:57 +0300

    Solution

---
+++ taxi/models.py
@@ -0,0 +1,66 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=63, unique=True)
+    country = models.CharField(max_length=63)
+
+    def __str__(self):
+        return f"{self.name} ({self.country})"
+
+
+class Driver(AbstractUser):
+    license_number = models.CharField(max_length=63)
+    password = models.CharField(max_length=63)
+
+    class Meta:
+        unique_together = (("license_number", "password"),)
+        verbose_name = "Driver"
+        verbose_name_plural = "Drivers"
+        ordering = ("license_number",)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Car(models.Model):
+    model = models.CharField(max_length=63)
+    manufacturer = models.ForeignKey(
+        Manufacturer,
+        on_delete=models.CASCADE,
+        related_name="cars"
+    )
+    drivers = models.ManyToManyField(Driver, related_name="cars")
+
+    class Meta:
+        ordering = ("model", )
+        verbose_name = "Car"
+        verbose_name_plural = "Cars"
+
+    def __str__(self):
+        return f"{self.model} {self.manufacturer.name}"
+
+
+# class Manufacturer(models.Model):
+#     name = models.CharField(max_length=100)
+#     country = models.CharField(max_length=100)
+#
+#     def __str__(self):
+#         return f"{self.name} ({self.country})"
+#
+#
+# class Driver(AbstractUser):
+#     license_number = models.CharField(max_length=100, unique=True)
+#
+#     def __str__(self):
+#         return f"{self.first_name} {self.last_name}"
+#
+#
+# class Car(models.Model):
+#     model = models.CharField(max_length=100)
+#     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, related_name="cars")
+#     drivers = models.ManyToManyField(Driver, related_name="cars")
+#
+#     def __str__(self):
+#         return f"{self.model} by {self.manufacturer.name}"
